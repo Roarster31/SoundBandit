68d8a1c6db09fc01efa302eb34f8e509f66aa34f
Rory Smith <roarsmith3@gmail.com>
2015-12-21 16:36:51 +0000

    Removed some fields we don't need in requests.

--- mp3lookup.sh
+++ mp3lookup.sh
@@ -4,11 +4,14 @@ rm -rf temp.spx
 ffmpeg -i $1 -loglevel quiet temp.spx 2> /dev/null
 echo "File conversion complete."
 
-#clear file
+# Clear file
 echo "" > output.txt
 
-URL='http://search.midomi.com/v2/?method=search&type=identify&prebuffer=4.096'
-USER_AGENT='User-Agent: AppNumber=31 AppVersion=6.9.3b APIVersion=2.0.0 BRAND=lge MODEL=LG-D855'
+URL='http://search.midomi.com/v2/?method=search&type=identify'
+
+# We must pass an AppNumber in User-Agent. What it is doesn't matter, and other fields are
+# irrelevant too.
+USER_AGENT='User-Agent: AppNumber=31'
 
 curl -X POST -H 'Transfer-Encoding: chunked' -H "$USER_AGENT" -T temp.spx -o output.txt "$URL"
 
